5c605eba65e1ee3794192cb0c4a5c4463c513574
Zainab Omar <zainab.alshaikhli01@gmail.com>
2020-07-11 03:48:11 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,10 +6,11 @@ def line(line)
  if line.size==0
     puts "The line is currently empty."
   else
-  line.each do |name, index|
+ message= line.each do |name, index|
     puts "The line is currently: #{index+1}. #{name}"
   end
 end
+return message
 end
 
 
