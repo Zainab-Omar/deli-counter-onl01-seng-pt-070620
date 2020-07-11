748b4f59fe0e5a65d6f2975f66b1ccf3b2902637
Zainab Omar <zainab.alshaikhli01@gmail.com>
2020-07-11 03:53:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,17 +2,5 @@
 
 katz_deli = []
 
-def line(line)
-  message = ""
-  if (line.size == 0)
-    puts "The line is currently empty."
-  else
-    message = "The line is currently:"
-    line.each_with_index do |customer, index|
-      position = index + 1
-      message.push (" #{position}. #{customer}")
-    end
-    puts message
-  end
-end
+
 
